4f690c4bde9ab589a3dfea4e4e94cdd0670a6845
Hm <hm@oh-lang.org>
2025-04-20 05:18:24 -0700

    move equality checking to test.h

--- test/reference_offsets.c
+++ test/reference_offsets.c
@@ -61,8 +61,6 @@
         return stack->data[--stack->count]; \
     } \
 
-#define float_t float
-
 void enscope_float_t_(float_t *f)
 {   *f = 0.0;
 }
@@ -73,25 +71,7 @@ void descope_float_t_(float_t *f)
 
 STACK(float_t)
 
-int equal_uint32_t_(uint32_t *a, uint32_t *b)
-{   return *a == *b;
-}
-void print_uint32_t_(FILE *f, uint32_t *uint32)
-{   fprintf(f, "%d", *uint32);
-}
-
-int equal_float_t_(float_t *a, float_t *b)
-{   float abs_delta = fabs(*a - *b);
-    float abs_min = fmin(fabs(*a), fabs(*b));
-    if (abs_min > 0.0) {
-        return abs_delta / abs_min < 1e-5;
-    }
-    // if zero, then require absoluteness
-    return *a == *b;
-}
-void print_float_t_(FILE *f, float_t *flt)
-{   fprintf(f, "%f", *flt);
-}
+TEST_C
 
 int main()
 {   stack_float_t stack;
--- test/test.h
+++ test/test.h
@@ -1,5 +1,7 @@
 #pragma once
 
+typedef float float_t;
+
 #define ASSERT_EQUAL(type_t, a, b) \
 {   type_t AE_a = (a); \
     type_t AE_b = (b); \
@@ -12,3 +14,24 @@
         exit(1); \
     } \
 }
+
+#define TEST_C \
+    int equal_uint32_t_(uint32_t *a, uint32_t *b) \
+    {   return *a == *b; \
+    } \
+    void print_uint32_t_(FILE *f, uint32_t *uint32) \
+    {   fprintf(f, "%d", *uint32); \
+    } \
+    int equal_float_t_(float_t *a, float_t *b) \
+    {   float abs_delta = fabs(*a - *b); \
+        float abs_min = fmin(fabs(*a), fabs(*b)); \
+        if (abs_min > 0.0) { \
+            return abs_delta / abs_min < 1e-5; \
+        } \
+        /* if zero, then require absoluteness */ \
+        return *a == *b; \
+    } \
+    void print_float_t_(FILE *f, float_t *flt) \
+    {   fprintf(f, "%f", *flt); \
+    } \
+    /* TEST_C end */
